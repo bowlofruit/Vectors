6e20f10a46d01e96ae3e6fe3c9f7a0e83a12561f
bowlofruit <ser150448@gmail.com>
2022-11-11 22:37:39 +0200

    ver.0.0.0.1

---
+++ Assets/MoveToGoal.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class MoveToGoal : MonoBehaviour {
+
+    public float speed = 2.0f;
+    public float accuracy = 0.01f;
+    public Transform goal;
+
+    void LateUpdate() {
+
+        this.transform.LookAt(goal.position);
+        Vector3 direction = goal.position - this.transform.position;
+        Debug.DrawRay(this.transform.position, direction, Color.red);
+        if (direction.magnitude > accuracy)
+        {
+           this.transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+        }
+    }
+}
